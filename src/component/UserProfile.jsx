fdc3886e063012b0877f4e89c2380aa457fdbc3e
Angel Rusev <i517171@FontysICT.nl>
2024-06-09 05:46:10 +0200

    d

---
+++ src/component/UserProfile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../assets/SideNav.css'
+import {Link} from "react-router-dom";
+import SideNav from "./SideNav";
+
+const UserProfile = () => {
+    return (
+        <div className="container-fluid vh-100 w-100 bg-gradient rounded-5 border-top border-danger overflow-hidden">
+            {/*<SideNav/>*/}
+            <SideNav>
+
+            </SideNav>
+        </div>
+    );
+};
+
+export default UserProfile;
